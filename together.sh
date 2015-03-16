44361259c28975351d3be0e440c1a784ce894372
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2015-03-15 19:07:06 -0500

    dump things together

---
+++ together.sh
@@ -0,0 +1,3 @@
+pdftk cover_letter.pdf CV_ForeroRomero.pdf research.pdf papers.pdf proceedings.pdf grants.pdf mentor.pdf all_papers_uniandes.pdf teaching.pdf cursos.pdf ProgramaCBU_AstronomiaPopular.pdf ProgramaHerramientasComputacionales.pdf ProgramaMetodosComputacionales201420.pdf ProgramaMetodosAvanzados.pdf service.pdf talks.pdf cat output tenure_file.pdf
+
+
